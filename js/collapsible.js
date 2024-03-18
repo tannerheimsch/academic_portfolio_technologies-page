3d5a36e2434f920d2acf09a466f0eb634edde3cf
tannerheimsch <h3imscht@outlook.com>
2024-03-18 04:41:02 -0500

    fixed button text alignment, general format clean

--- js/collapsible.js
+++ js/collapsible.js
@@ -3,19 +3,19 @@ const collapsibleButtons = document.querySelectorAll('.collapsible');
 collapsibleButtons.forEach(button => {
     button.addEventListener('click', function() {
         const buttonTopOffset = button.getBoundingClientRect().top + window.scrollY;
-
         this.classList.toggle('active');
         const content = this.nextElementSibling;
         content.classList.toggle('active');
 
         if (content.classList.contains('active')) {
             content.style.maxHeight = content.scrollHeight + "px";
-            content.addEventListener('transitionend', function() {
+            content.addEventListener('transitionend', function transitionHandler() {
                 window.scrollTo({
                     top: buttonTopOffset,
                     behavior: 'smooth'
                 });
-            }, { once: true });
+                content.removeEventListener('transitionend', transitionHandler);
+            });
         } else {
             content.style.maxHeight = null;
         }
